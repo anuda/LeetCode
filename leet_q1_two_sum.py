ae267e7335493c0f765b2204c1eb06ce4f67c12a
anuda <anurag2das@gmail.com>
2021-09-15 23:20:02 +0530

    Update leet_q1_two_sum.py

--- leet_q1_two_sum.py
+++ leet_q1_two_sum.py
@@ -3,7 +3,7 @@ class Solution:
         pass
         
     
-    def twoSum(self, nums, target):
+    def twoSum_1(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
@@ -20,3 +20,17 @@ class Solution:
                     nums.remove(j)
                     return([i,nums.index(diff)+1])
             
+    def twoSum_2(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        Runtime: 1274 ms, faster than 40.91% of Python online submissions for Two Sum.
+        Memory Usage: 14.2 MB, less than 73.01% of Python online submissions for Two Sum.
+        """
+        
+        for i,j in enumerate(nums):
+            diff = target-j
+            if diff in nums[:i]+nums[i+1:]:
+                nums.reverse()
+                return(i,len(nums)-nums.index(diff)-1)
